74c8f1c0feb59a3c19ac0833dcd5126cb2a3e82c
jayesh-intern-probo <jayesh@probo.in>
2023-01-07 02:26:45 +0530

    Added Date Picker Dialog

--- app/src/main/java/com/probo/androidassignment/activities/SignUpActivity.kt
+++ app/src/main/java/com/probo/androidassignment/activities/SignUpActivity.kt
@@ -1,12 +1,62 @@
 package com.probo.androidassignment.activities
 
+import android.app.DatePickerDialog
+import android.icu.util.Calendar
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.annotation.RequiresApi
+import androidx.appcompat.widget.AppCompatEditText
+import androidx.appcompat.widget.AppCompatImageButton
 import com.probo.androidassignment.R
+import java.text.SimpleDateFormat
+import java.util.*
 
 class SignUpActivity : AppCompatActivity() {
+
+    private var etSelectedDate: AppCompatEditText? = null
+
+    @RequiresApi(Build.VERSION_CODES.N)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
+
+        val dobButton = findViewById<AppCompatImageButton>(R.id.dob_picker)
+        etSelectedDate = findViewById<AppCompatEditText>(R.id.et_dob)
+        dobButton.setOnClickListener {
+            pickDateFromDatePickerDialog()
+        }
+    }
+
+    @RequiresApi(Build.VERSION_CODES.N)
+    private fun pickDateFromDatePickerDialog() {
+        val myCalendar = Calendar.getInstance()
+        val year = myCalendar.get(Calendar.YEAR)
+        val month = myCalendar.get(Calendar.MONTH)
+        val day = myCalendar.get(Calendar.DAY_OF_MONTH)
+
+        val dpd = DatePickerDialog(this,
+            DatePickerDialog.OnDateSetListener{ _, selectedYear, selectedMonth, selectedDay ->
+                val selectedDate = "$selectedDay-${selectedMonth+1}-${selectedYear}"
+                etSelectedDate?.isEnabled = true
+                etSelectedDate?.setText(selectedDate)
+
+                val sdf = SimpleDateFormat("dd-MM-yyyy", Locale.ENGLISH)
+
+                val theDate = sdf.parse(selectedDate)
+                theDate?.let {
+                    val selectedDateInMinutes = theDate.time / 60000
+
+                    val currentDate = sdf.parse(sdf.format(System.currentTimeMillis()))
+
+                    currentDate?.let {
+                        val currentDateInMinutes = currentDate.time / 60000
+                    }
+                }
+
+              }, year, month, day)
+
+        dpd.datePicker.maxDate = System.currentTimeMillis() - 86400000
+        dpd.show()
     }
 }
