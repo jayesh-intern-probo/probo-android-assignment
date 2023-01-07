6b495482e8f8b3da3b655dbdae2032ae5c7aedc1
jayesh-intern-probo <jayesh@probo.in>
2023-01-07 18:20:07 +0530

    Validation Complete

--- app/src/main/java/com/probo/androidassignment/activities/SignUpActivity.kt
+++ app/src/main/java/com/probo/androidassignment/activities/SignUpActivity.kt
@@ -1,16 +1,23 @@
 package com.probo.androidassignment.activities
 
 import android.app.DatePickerDialog
+import android.content.Intent
 import android.icu.util.Calendar
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.LinearLayout
+import android.widget.Toast
 import androidx.annotation.RequiresApi
+import androidx.appcompat.widget.AppCompatButton
 import androidx.appcompat.widget.AppCompatEditText
 import androidx.appcompat.widget.AppCompatImageButton
+import androidx.core.content.ContextCompat
+import com.google.android.material.snackbar.Snackbar
 import com.probo.androidassignment.R
 import java.text.SimpleDateFormat
 import java.util.*
+import java.util.regex.Pattern
 
 class SignUpActivity : AppCompatActivity() {
 
@@ -22,9 +29,14 @@ class SignUpActivity : AppCompatActivity() {
         setContentView(R.layout.activity_sign_up)
 
         val dobButton = findViewById<AppCompatImageButton>(R.id.dob_picker)
-        etSelectedDate = findViewById<AppCompatEditText>(R.id.et_dob)
+        etSelectedDate = findViewById(R.id.et_dob)
         dobButton.setOnClickListener {
             pickDateFromDatePickerDialog()
+        }
+
+        val btnSignUp = findViewById<AppCompatButton>(R.id.btn_sign_up)
+        btnSignUp.setOnClickListener {
+            validateInput()
         }
     }
 
@@ -38,7 +50,6 @@ class SignUpActivity : AppCompatActivity() {
         val dpd = DatePickerDialog(this,
             DatePickerDialog.OnDateSetListener{ _, selectedYear, selectedMonth, selectedDay ->
                 val selectedDate = "$selectedDay-${selectedMonth+1}-${selectedYear}"
-                etSelectedDate?.isEnabled = true
                 etSelectedDate?.setText(selectedDate)
 
                 val sdf = SimpleDateFormat("dd-MM-yyyy", Locale.ENGLISH)
@@ -51,6 +62,10 @@ class SignUpActivity : AppCompatActivity() {
 
                     currentDate?.let {
                         val currentDateInMinutes = currentDate.time / 60000
+
+                        val difference = currentDateInMinutes - selectedDateInMinutes
+                        if(difference < 18*365*24*60)
+                            showSnackBar("Valid For 18+ Years Of Age")
                     }
                 }
 
@@ -58,5 +73,49 @@ class SignUpActivity : AppCompatActivity() {
 
         dpd.datePicker.maxDate = System.currentTimeMillis() - 86400000
         dpd.show()
+    }
+
+    private fun validateInput() {
+        val etEmail = findViewById<AppCompatEditText>(R.id.et_email).text.toString().trim()
+        val etPassword = findViewById<AppCompatEditText>(R.id.et_password).text.toString()
+        val etRePassword = findViewById<AppCompatEditText>(R.id.et_re_password).text.toString()
+
+
+        if(etEmail.isNotEmpty()) {
+            if(etEmail.matches("[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+".toRegex())) {
+                if(etPassword.isNotEmpty() && etRePassword.isNotEmpty()) {
+                    if(etPassword == etRePassword) {
+                        if(etPassword.matches("^(?=.*[0-9])(?=.*[A-Z])(?=.*[@#$%^&+=!])(?=\\S+$).{4,}$".toRegex())) {
+                            val intent = Intent(this@SignUpActivity, MainActivity::class.java)
+
+                            val bundle = Bundle()
+                            bundle.putString("email", etEmail)
+                            bundle.putString("dob", etSelectedDate?.text.toString())
+                            bundle.putString("password", etPassword)
+
+                            intent.putExtras(bundle)
+                            startActivity(intent)
+                        }
+                        else showSnackBar("Password must contain 8 letters(a capital and a small alphabet, a digit & one special symbol)")
+                    }
+                    else showSnackBar("Passwords Doesn't Match")
+                }
+                else showSnackBar("Passwords Field Can't Be Empty")
+            }
+            else showSnackBar("Invalid Email Format")
+        }
+        else showSnackBar("Email Field Can't Be Empty")
+    }
+
+    private fun showSnackBar(message: String) {
+        val snackBar = Snackbar.make(findViewById(R.id.content_main), message, Snackbar.LENGTH_SHORT)
+        val snackBarView = snackBar.view
+        snackBarView.setBackgroundColor(
+            ContextCompat.getColor(
+                this@SignUpActivity,
+                R.color.snackBarErrorColor
+            )
+        )
+        snackBar.show()
     }
 }
